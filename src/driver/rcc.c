0166e47640dc55a997cfa51b8904ecaf4bece576
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-11-24 19:16:36 -0300

    defined enable and disable clock structures

--- src/driver/rcc.c
+++ src/driver/rcc.c
@@ -53,6 +53,43 @@ void enableRCC(const enum RCC_PERIPHERAL rcc_per) {
 void diableRCC(const enum RCC_PERIPHERAL rcc_per) {
 
 
-
+    switch (rcc_per) {
+        case SYS_CFG:
+            // Disable here the RCC register for SYS_CFG
+            break;
+        case GPIO_A_PER:
+            // Disable here the RCC register for GPIO_A_PER
+            break;
+        case GPIO_B_PER:
+            // Disable here the RCC register for GPIO_B_PER
+            break;
+        case GPIO_C_PER:
+            // Disable here the RCC register for GPIO_C_PER
+            break;
+        case GPIO_D_PER:
+            // Disable here the RCC register for GPIO_D_PER
+            break;
+        case GPIO_E_PER:
+            // Disable here the RCC register for GPIO_E_PER
+            break;
+        case I2C_PER:
+            // Disable here the RCC register for I2C_PER
+            break;
+        case SPI_PER:
+            // Disable here the RCC register for SPI_PER
+            break;
+        case TIM1_PER:
+            // Disable here the RCC register for TIM1_PER
+            break;
+        case EXTI_PER:
+            // Disable here the RCC register for EXTI_PER
+            break;
+        case LCD_PER:
+            // Disable here the RCC register for LCD_PER
+            break;
+        default:
+            // Handle unknown value
+            break;
+    }
 
 }
