282df3e9a96cf2e109aa0df71ea9e597e09fadf5
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-12-28 11:30:34 -0300

    added more error codes

--- include/etc/error.h
+++ include/etc/error.h
@@ -5,10 +5,16 @@
 #ifndef OPENCAS_ERROR_H
 #define OPENCAS_ERROR_H
 
-enum ERROR_CODE {
-      RCC_CFG_ERR             = 1,
-      GPIO_CFG_ERR            = 2,
-      I2C_CFG_ERR             = 6
+enum CORE_ERROR_CODE {
+      RCC_CFG_ERR             = 1, // threw under the condition of failure in enabling the clock of the STM
+      GPIO_CFG_ERR            = 2, // threw under the condition of a GPIO init failure
+      I2C_CFG_ERR             = 3, // threw under the condition of a I2C init failure
+};
+
+enum PER_ERROR_CODE {
+      RTC_CFG_ERR             = 1, // threw under an init failure condition
+      RTC_ALARM_CFG_ERR       = 2, // threw under a failure to configure the alarm
+      RTC_TIMER_CFG_ERR       = 3  // threw under a failure to configure the timer
 };
 
 #endif //OPENCAS_ERROR_H
